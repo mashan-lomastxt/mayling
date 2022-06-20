80dbd38026681774558fb1d934a852313dc11b17
Dzakirz <dzakirz.dev@gmail.com>
2022-06-20 16:41:59 +0700

    add axios react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from "react"
+import React from 'react'
+import axios from 'axios'
 
 const App = () => {
+    const [movies,setMovies] = React.useState([])
+
+    const getMovies = async() => {
+        const response = await axios.get("https://api.themoviedb.org/3/movie/550?api_key=2588072e53fb37c608b4b2a6cc38fe9f", {
+            params: {
+                page: 1
+            }
+        })
+        setMovies(response.data.results)
+    }
+
+    React.useEffect(() => {
+        getMovies()
+    }, [])
+
+    console.log(movies)
 
     return(
         <div>
